119db53deb251399e0499089bae4cdb92431fde8
jariftahmid <jariftahmid10@gmail.com>
2026-02-12 12:19:24 +0600

    r

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,19 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 document.getElementById("loginBtn").onclick = async () => {
   const email = document.getElementById("loginEmail").value;
   const pass = document.getElementById("loginPass").value;
 
   const userCred = await signInWithEmailAndPassword(auth,email,pass);
-  const token = await userCred.user.getIdTokenResult();
-  if(token.claims.admin) window.location.href="admin.html";
-  else window.location.href="client.html";
+
+  // 🔥 Firestore admin check
+  const adminDoc = await getDoc(doc(db,"admins",email));
+
+  if(adminDoc.exists()){
+    window.location.href="admin.html";
+  }else{
+    window.location.href="client.html";
+  }
 };
